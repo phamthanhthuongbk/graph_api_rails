acf0af6ba7e7ed5413f62d7fc1fcb2f9a232309c
pham_thanh_thuong <pham_thanh_thuong@uniquevision.co.jp>
2020-05-31 21:59:15 +0900

    Add fields

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::PostsController < ApplicationController
 
     def show
       # @post is already loaded and authorized
-      render json: @post, status: :ok
+      render json: render_item(@post, fields), status: :ok
     end
 
     def index
       # @posts is already loaded with all posts the user is authorized to read
-      render json: @posts, status: :ok
+      render json: render_items(@posts, fields), status: :ok
     end
   end
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,8 +4,29 @@ class ApplicationController < ActionController::API
     end
 
     def fields
-        params.permit(
-            :fields,
-        )
+        params.permit!.to_h["fields"]
+    end
+
+    def render_item(item, fields)
+      return {} if item.blank?
+      return item if fields.blank?
+      json_data = {}
+      for field in fields do
+        if field.is_a? String
+          json_data[field] = (item.class.method_defined? field) ? item.send(field) : nil
+        end
+
+        if field.is_a? Hash
+          field_name = field.keys.first
+          values = field.values.first
+          json_data[field_name] = (item.class.method_defined? field_name) ? render_item(item.send(field_name), values) : nil
+        end
+      end
+
+      json_data
+    end
+
+    def render_items(items, fields)
+      items.map { | item | render_item(item, fields) }
     end
 end

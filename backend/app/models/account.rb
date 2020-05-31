ccbf8e2c97e0a4bf09c62fab16e76028d54579f1
pham_thanh_thuong <pham_thanh_thuong@uniquevision.co.jp>
2020-05-31 20:27:21 +0900

    Add lint model

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -1,2 +1,6 @@
 class Account < ApplicationRecord
+    has_many :comments, dependent: :destroy
+    has_many :reactions, dependent: :destroy
+    has_many :images, dependent: :destroy
+    has_many :posts, dependent: :destroy
 end
--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :account
+    has_many :comments, dependent: :destroy
+    has_many :reactions, dependent: :destroy
 end

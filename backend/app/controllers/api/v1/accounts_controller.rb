fdc6b61da515aefdc195b0355f8fc980d3f646fa
pham_thanh_thuong <pham_thanh_thuong@uniquevision.co.jp>
2020-05-31 22:01:25 +0900

    Add API for account

--- backend/app/controllers/api/v1/accounts_controller.rb
+++ backend/app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::AccountsController < ApplicationController
+    load_and_authorize_resource
+
+    def show
+      render json: render_item(@account, fields), status: :ok
+    end
+
+    def index
+      render json: render_items(@accounts, fields), status: :ok
+    end
 end
